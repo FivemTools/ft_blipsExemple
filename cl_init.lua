b37737307dd7fd2871ef825dc2ac346c784e18bc
samuelds <samuelds.pro@gmail.com>
2017-06-14 21:45:45 +0200

    Update

--- cl_init.lua
+++ cl_init.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-06-11T23:20:51+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-13T15:03:46+02:00
+-- @Last modified time: 2017-06-14T21:30:17+02:00
 -- @License: GNU General Public License v3.0
 
 -- Load client at 100%
@@ -12,7 +12,7 @@ Citizen.CreateThread(function()
     if NetworkIsSessionStarted() then
 
       -- Send menu to Menu Generator
-      exports.ft_blips:BlipGenerator(blips)
+      exports.ft_blips:Generator(blips)
       break
 
     end
